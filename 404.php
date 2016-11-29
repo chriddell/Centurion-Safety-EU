65f5f84fe63f327ca5b1f8e0282212bf03b236ec
Chris Liddell <chrisliddell1991@gmail.com>
2016-11-29 17:35:07 +0000

    Created 404 page.

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+	<h1>404!</h1>
+
+<?php get_footer(); ?>
