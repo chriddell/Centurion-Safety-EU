7fdd6c9674ac0829bfd5b0532618a084e003b24d
Chris Liddell <chrisliddell1991@gmail.com>
2016-12-01 11:09:29 +0000

    https://trello.com/c/2NZuYho9/40-build-stockists-page

---
+++ page-stockists.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Stockists page
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Centurion
+ */
+
+get_header(); ?>
+
+
+
+<?php
+// Show WP-Google-Map
+echo do_shortcode('[wpgmza id="1"]'); ?>
+
+<?php get_footer(); ?>
