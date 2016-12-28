b91c25d740e4e03e311692159e5a7cd31be4092b
Chris Liddell <chrisliddell1991@gmail.com>
2016-12-28 13:50:00 +0000

    https://trello.com/c/kndv5ucu/176-allow-check-uncheck-of-filters-by-clicking-anywhere-on-element

--- template-parts/content-search-product.php
+++ template-parts/content-search-product.php
@@ -7,16 +7,18 @@
  * @subpackage Centurion
  * @since Centurion 1.0
  */
+
+// ACF Fields
+$product_images = get_field('product_images');
+
 ?>
 
-<div class="col-12 col-sml-4">
-	<article class="search-result search-result--product product-listing product-listing--centered product-listing--search">
-		<div class="product-listing__container">
-			<div class="product-listing__image-container">
-				<img src="<?php echo get_field('product_images')[0]['product_image']['url']; ?>" class="product-listing__image"/>
-			</div>
-			<?php the_title('<h3 class="product-listing__title">', '</h3>'); ?>
-			<a href="<?php echo get_permalink(); ?>" class="product-listing__link"><?php _e('More info', 'centurion' ); ?><span class="icon icon--link"></a>
+<article class="search-result search-result--product product-listing product-listing--centered product-listing--search col-12 col-sml-4">
+	<div class="product-listing__container">
+		<div class="product-listing__image-container">
+			<img src="<?php echo $product_images[0]['product_image']['url']; ?>" class="product-listing__image"/>
 		</div>
-	</article>
-</div>
+		<?php the_title('<h3 class="product-listing__title">', '</h3>'); ?>
+		<a href="<?php echo get_permalink(); ?>" class="product-listing__link"><?php _e('More info', 'centurion' ); ?><span class="icon icon--link"></a>
+	</div>
+</article>
