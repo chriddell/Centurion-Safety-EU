00459b3cc2e93becb8825edd905d1fb10fdb32b6
Chris Liddell <chrisliddell1991@gmail.com>
2016-12-29 12:56:35 +0000

    https://trello.com/c/whiNqdX6/191-ensure-events-are-rendering-properly-on-news

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -20,5 +20,7 @@ $eventpost = array(
 	<?php the_title('<h4 class="event__title">', '</h4>'); ?>
 	<p class="event__text event__text--date"><?php echo $eventpost['date']->format('j F Y'); ?></p>
 	<p class="event__text event__text--location"><?php echo $eventpost['location']; ?></p>
-	<a href="<?php echo $eventpost['url']; ?>" class="event__link" target="_blank">Visit website</a>
+	<?php if ( $eventpost['url'] ) { ?>
+		<a href="<?php echo $eventpost['url']; ?>" class="event__link" target="_blank">Visit website</a>
+	<?php } ?>
 </li>
